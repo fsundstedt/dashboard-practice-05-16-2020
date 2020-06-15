7f866ccc11055c7234bd2040b817eb1d0808b293
unknown <frank.sundstedt@gmail.com>
2020-06-15 12:21:19 -0400

    practicing C# basics

---
+++ Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace _05_16_2020_dashboard
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.countyButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(174, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Zillow Main Search";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.AutoSize = true;
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.comboBox2);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Location = new System.Drawing.Point(31, 71);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.groupBox1.Size = new System.Drawing.Size(350, 101);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(193, 40);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 1;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(34, 40);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // countyButton
+            // 
+            this.countyButton.Location = new System.Drawing.Point(387, 109);
+            this.countyButton.Name = "countyButton";
+            this.countyButton.Size = new System.Drawing.Size(148, 23);
+            this.countyButton.TabIndex = 2;
+            this.countyButton.Text = "Select a County";
+            this.countyButton.UseVisualStyleBackColor = true;
+            this.countyButton.Click += new System.EventHandler(this.countyButton_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(145, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "County GIS";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(594, 450);
+            this.Controls.Add(this.countyButton);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button countyButton;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,259 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _05_16_2020_dashboard
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.Items.Add("Georgia");
+            comboBox1.Items.Add("North Carolina");
+            comboBox1.Items.Add("Tennessee");
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            Process.Start("C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe",
+            "https://www.zillow.com/homes/for_sale/?searchQueryState=%7B%22pagination%22%3A%7B%7D%2C%22" +
+            "mapBounds%22%3A%7B%22west%22%3A-87.17266864731548%2C%22east%22%3A-80.74566669419048%2C%22south%22%3A32.962499414402274%2C%22north%22%3A36.72119073932386%7D%2C%22isMapVisible%22%3Atrue%2C%22mapZoom%22%3A8%2C%22filterState%22%3A%7B%22" +
+            "price%22%3A%7B%22min%22%3A0%2C%22max%22%3A200000%" +
+            "7D%2C%22mp%22%3A%7B%22min%22%3A0%2C%22max%22%3A714%7D%2C%22sort%22%3A%7B%22value%22%3A%22" +
+            "lot%22%7D%2C%22lot%22%3A%7B%22min%22%3A2178000%7D%7D%2C%22isListVisible%22%3Atrue%2C%22customRegionId%22%3A%220aa965ebeeX1-CR1ngqaivvkzgxq_zwat1%22%7D");
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string[] georgiaCounties = {
+                "Dawson County",
+                "Fannin County",
+                "Gilmer County",
+                "Habersham County",
+                "Lumpkin County",
+                "Pickins County",
+                "Rabun County",
+                "Stephens County",
+                "Towns County",
+                "Union County",
+                "White County"
+            };
+
+            string[] ncCounties = {
+                "Alleghany County",
+                "Ashe County",
+                "Avery County",
+                "Buncombe County",
+                "Burke County",
+                "Caldwell County",
+                "Cherokee County",
+                "Clay County",
+                "Graham County",
+                "Haywood County",
+                "Henderson County",
+                "Jackson County",
+                "Macon County",
+                "Madison County",
+                "McDowell County",
+                "Mitchell County",
+                "Polk County",
+                "Rutherford County",
+                "Swain County",
+                "Transylvania County",
+                "Watauga County",
+                "Wilkes County",
+                "Yancey County",
+            };
+
+            string[] tnCounties = {
+                "All Counties",
+            };
+
+            comboBox2.Items.Clear();
+
+            string stateInput = comboBox1.Text;
+
+            if (stateInput == "Georgia")
+            {
+                foreach (string county in georgiaCounties)
+                {
+                    comboBox2.Items.Add(county);
+                }
+            }
+            else if (stateInput == "North Carolina")
+            {
+                foreach (string county in ncCounties)
+                {
+                    comboBox2.Items.Add(county);
+                }
+            }
+            else if (stateInput == "Tennessee")
+            {
+                foreach (string county in tnCounties)
+                {
+                    comboBox2.Items.Add(county);
+                }
+            }
+        }
+
+        private void countyButton_Click_1(object sender, EventArgs e)
+        {
+            string[] georgiaCounties = {
+                "Dawson County",
+                "Fannin County",
+                "Gilmer County",
+                "Habersham County",
+                "Lumpkin County",
+                "Pickins County",
+                "Rabun County",
+                "Stephens County",
+                "Towns County",
+                "Union County",
+                "White County"
+            };
+
+            string[] ncCounties = {
+                "Alleghany County",
+                "Ashe County",
+                "Avery County",
+                "Buncombe County",
+                "Burke County",
+                "Caldwell County",
+                "Cherokee County",
+                "Clay County",
+                "Graham County",
+                "Haywood County",
+                "Henderson County",
+                "Jackson County",
+                "Macon County",
+                "Madison County",
+                "McDowell County",
+                "Mitchell County",
+                "Polk County",
+                "Rutherford County",
+                "Swain County",
+                "Transylvania County",
+                "Watauga County",
+                "Wilkes County",
+                "Yancey County",
+            };
+
+            string[] tnCounties = {
+                "All Counties",
+            };
+
+            string[] georgiaCountySites = {
+                "Dawson County",
+                "Fannin County",
+                "Gilmer County",
+                "Habersham County",
+                "Lumpkin County",
+                "Pickins County",
+                "Rabun County",
+                "Stephens County",
+                "Towns County",
+                "Union County",
+                "White County"
+            };
+
+            string[] ncCountySites = {
+                "Alleghany County",
+                "Ashe County",
+                "Avery County",
+                "Buncombe County",
+                "Burke County",
+                "Caldwell County",
+                "https://maps.cherokeecounty-nc.gov/GISweb/GISviewer/",
+                "Clay County",
+                "http://grahamconc.maps.arcgis.com/apps/Solutions/s2.html?appid=2a8aac7f18164a0389472dbe52f68ee0",
+                "Haywood County",
+                "Henderson County",
+                "Jackson County",
+                "Macon County",
+                "Madison County",
+                "McDowell County",
+                "Mitchell County",
+                "Polk County",
+                "Rutherford County",
+                "Swain County",
+                "Transylvania County",
+                "Watauga County",
+                "Wilkes County",
+                "Yancey County",
+            };
+
+            string[] tnCountySites = {
+                "https://tnmap.tn.gov/assessment/",
+
+            };
+
+            string buttonInput = countyButton.Text;
+            string[] counties = georgiaCounties;
+            string[] stateSite = georgiaCountySites;
+
+            if (comboBox1.Text == "Georgia")
+            {
+                counties = georgiaCounties;
+            }
+            else if (comboBox1.Text == "North Carolina")
+            {
+                counties = ncCounties;
+            }
+            else if (comboBox1.Text == "Tennessee")
+            {
+                counties = tnCounties;
+            }
+
+            if (comboBox1.Text == "Georgia")
+            {
+                stateSite = georgiaCountySites;
+            } 
+            else if (comboBox1.Text == "North Carolina")
+            {
+                stateSite = ncCountySites;
+            }
+            else if (comboBox1.Text == "Tennessee")
+            {
+                stateSite = tnCountySites;
+            }
+
+
+            if (stateSite[Array.IndexOf(counties, buttonInput)].Contains("http"))
+            {
+                Process.Start("C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe",
+                $"{stateSite[Array.IndexOf(counties, buttonInput)]}");
+            } else
+            {
+                Process.Start("C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe",
+                "https://www.google.com/");
+            }
+
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            countyButton.Text = comboBox2.Text;
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
